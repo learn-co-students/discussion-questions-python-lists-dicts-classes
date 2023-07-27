c588dcd00fb8e9512269e894b813043fa8ad6360
betalantz <betalantz@gmail.com>
2023-07-27 09:16:21 -0700

    solution to car quetions

--- debug.py
+++ debug.py
@@ -2,4 +2,8 @@ import ipdb
 
 from lib.review_question_car_class import Car
 
+volvo_lightning = Car("Volvo", "Lightning")
+yugo = Car("Zastava", "Yugo")
+lada = Car("AvtoVAZ", "Lada")
+
 ipdb.set_trace()
--- lib/review_question_car_class.py
+++ lib/review_question_car_class.py
@@ -2,32 +2,45 @@
 
 
 class Car:
-    pass
+    all = []
+
+    @classmethod
+    def brake(cls):
+        print("SREEEEEEECH!")
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+        Car.all.append(self)
+
+    def drive(self):
+        print(f"{self.model} goes VRRRROOOOM!")
 
 
-volvo_lightning = Car("Volvo", "Lightning")
-yugo = Car("Zastava", "Yugo")
-lada = Car("AvtoVAZ", "Lada")
+if __name__ == "__main__":
+    volvo_lightning = Car("Volvo", "Lightning")
+    yugo = Car("Zastava", "Yugo")
+    lada = Car("AvtoVAZ", "Lada")
 
-volvo_lightning.make
-# => "Volvo"
-volvo_lightning.model
-# => "Lightning"
+    volvo_lightning.make
+    # => "Volvo"
+    volvo_lightning.model
+    # => "Lightning"
 
-yugo.drive()
-# => "VROOOOOOOOOOOOM!"
+    yugo.drive()
+    # => "VROOOOOOOOOOOOM!"
 
-Car.all
-# => [<__main__.Car object at 0x1049247c0>, <__main__.Car object at 0x1049247c8>, <__main__.Car object at 0x1049247d6>]
+    Car.all
+    # => [<__main__.Car object at 0x1049247c0>, <__main__.Car object at 0x1049247c8>, <__main__.Car object at 0x1049247d6>]
 
-# BONUS:
+    # BONUS:
 
-Car.brake()
-# => "SCREEEECH!"
+    Car.brake()
+    # => "SCREEEECH!"
 
-volvo_lightning = Car(make="Volvo", model="Lightning")
+    volvo_lightning = Car(make="Volvo", model="Lightning")
 
-volvo_lightning.make
-# => "Volvo"
-volvo_lightning.model
-# => "Lightning"
+    volvo_lightning.make
+    # => "Volvo"
+    volvo_lightning.model
+    # => "Lightning"
